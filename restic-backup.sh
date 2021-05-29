cf25d86fe585626590ab1f793b0191e560efd1e6
calr0x <52664000+calr0x@users.noreply.github.com>
2021-05-29 13:29:05 -0500

    Add files via upload

--- restic-backup.sh
+++ restic-backup.sh
@@ -36,7 +36,7 @@ if [ $STATUS == 1 ]; then
 fi
 
 echo "Moving hidden data out of dated folder into backup"
-OUTPUT=$(mv -v /root/Smoothbrain-OT-Backup/backup/202*/.* /root/Smoothbrain-OT-Backup/backup/ 2>&1)
+OUTPUT=$(mv -v /root/Smoothbrain-OT-Backup/backup/.origintrail_noderc /root/Smoothbrain-OT-Backup/backup/ 2>&1)
 echo $STATUS
 if [ $STATUS == 1 ]; then
   /root/Smoothbrain-OT-Backup/data/send.sh "Moving data command FAILED${N1}$OUTPUT"
