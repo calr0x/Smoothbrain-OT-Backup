4c172b7d3980e37a469595470ee0f64beeeb2161
calr0x <52664000+calr0x@users.noreply.github.com>
2021-05-31 09:07:48 -0500

    Add files via upload

--- install-restic-repo.sh
+++ install-restic-repo.sh
@@ -5,75 +5,86 @@ N1=$'\n'
 
 clear
 
-KEY_ORIGINAL="export RESTIC_PASSWORD=\"REPLACE_WITH_RESTIC_REPOSITORY_PASSWORD\""
-KEY_CHECK=$(cat config.sh | grep REPLACE_WITH_RESTIC_REPOSITORY_PASSWORD)
+echo "Checking the configs to make sure they've been changed from defaults. We are ONLY checking to make sure they have \
+been CHANGED and not that they are VALID changes. It is important the URL of the storage (S3/B2) is correct.${N1}${N1}"
 
-if [ "$KEY_CHECK" != "$KEY_ORIGINAL" ]; then
-  read -p "There is already a key generated or the S3 key field is invalid. \
-  This installer will only run when the config.sh is set to default values. \
-  Press "r" to restore to the default config and "c" to skip key generation and continue:${N1}" -n 1 -r
-  if [[ $REPLY =~ ^[Cc]$ ]]; then
-    echo "Proceeding...${N1}"
-  else
-    cp config-original.sh config.sh
-  fi
-fi
-
-echo "Generating random passkey for repository"
-REPO_PASSKEY=$(apg -a 1 -m 32 -n 1 -M NCL)
-echo $REPO_PASSKEY
-
-echo "Copy this key to your password manager. You ARE using a password manager. Right? RIGHT?"
-read -p "Press enter to continue...${N1}"
-
-sed -i s,REPLACE_WITH_RESTIC_REPOSITORY_PASSWORD,$REPO_PASSKEY,g config.sh
-
-echo "Initializing the S3 repository"
-INIT_OUTPUT='/root/Smoothbrain-OT-Backup/restic init'
-INIT_STATUS=$?
-echo "$INIT_OUTPUT"
-
-if [ $INIT_STATUS == 0 ]; then
-  echo "Init SUCCEEDED"
-else
-  echo "Init FAILED:${N1}$INIT_OUTPUT"
+if [ $RESTIC_REPOSITORY == "REPLACE_WITH_S3_OR_B2_BUCKET_URL" ]; then
+  echo "${N1}${N1}The RESTIC_REPOSITORY value in config.sh has not been edited.${N1}\
+Please edit it to include the URL of either your Amazon S3 or Backblaze B2${N1}\
+bucket.${N1}${N1}Amazon: s3:https://s3.amazonaws.com/bucketname${N1}${N1}\
+Backblaze: b2:bucketname:path/to/repo${N1}"
   exit 1
 fi
 
-read -p "Are you running multiple servers and will be using ansible to deploy the backup software? Press "y" for yes and "n" for no...${N1}${N1}" -n 1 -r
+if [ $RESTIC_PASSWORD == "REPLACE_WITH_RESTIC_REPOSITORY_PASSWORD" ]; then
+  echo "${N1}${N1}The RESTIC_PASSWORD value in config.sh has not been edited. \
+Please edit it to include the password to use for the respository. As the data on \
+the repository WILL be encrypted, you will use this password to access the \
+repository.${N1}${N1}You MUST save this password somewhere. Losing this password will \
+prevent ALL ACCESS to the repository!${N1}"
+  exit 1
+fi
+
+read -s -p "Are you using S3 for your repository? Press \"y\" for S3 or \"n\" for B2:" -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-  read -p "${N1}Are you logging in as root? Press "y" if yes and "n" if logging in as a non-root user...${N1}" -n 1 -r
+  if [ $AWS_ACCESS_KEY_ID == "REPLACE_WITH_AWS_ACCESS_KEY" ]; then
+    echo "${N1}${N1}The AWS_ACCESS_KEY_ID value in config.sh has not been edited. \
+Please edit it to include the AWS access key ID to use to login to your S3 bucket.${N1}"
+    exit 1
+  fi
+  if [ $AWS_SECRET_ACCESS_KEY == "REPLACE_WITH_AWS_SECRET_ACCESS_KEY" ]; then
+    echo "${N1}${N1}The AWS_SECRET_ACCESS_KEY_ID value in config.sh has not been edited. \
+Please edit it to include the AWS secret access key ID to use to login to your S3 bucket.${N1}"
+    exit 1
+  fi
+else
+  if [ $B2_ACCOUNT_ID == "REPLACE_WITH_B2_ACCOUNT_ID" ]; then
+    echo "${N1}${N1}The B2_ACCOUNT_ID value in config.sh has not been edited. \
+Please edit it to include the Backblaze access key ID to use to login to your B2 bucket.${N1}"
+    exit 1
+  fi
+  if [ $B2_ACCOUNT_KEY == "REPLACE_WITH_B2_ACCOUNT_KEY" ]; then
+    echo "${N1}${N1}The B2_ACCOUNT_KEY value in config.sh has not been edited. \
+Please edit it to include the Backblaze  secret access key ID to use to login to your B2 bucket.${N1}"
+    exit 1
+  fi
+fi
+
+echo "${N1}${N1}Creating the S3 repository on S3/B2${N1}"
+/root/Smoothbrain-OT-Backup/restic -r $RESTIC_REPOSITORY init
+INIT_STATUS=$?
+
+if [ $INIT_STATUS == 0 ]; then
+  echo "Initializing the S3 repository: SUCCEEDED${N1}${N1}"
+else
+  echo "Initializing the S3 repository: FAILED${N1}${N1}"
+  exit 1
+fi
+
+echo "${N1}Installing cron schedule to run the backup every 6 hours.${N1}"
+crontab -l | grep -v '/root/Smoothbrain-OT-Backup/restic-backup.sh' | crontab -
+(crontab -l 2>/dev/null; echo "0 */6 * * * /root/Smoothbrain-OT-Backup/restic-backup.sh") | crontab -
+
+read -s -s -p "Will you be using ansible to deploy the backup software? Press "y" for yes and "n" for no...${N1}${N1}" -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  read -s -p "${N1}Are you logging in as root? Press "y" if yes and "n" if logging in as a non-root user...${N1}" -n 1 -r
   if [[ $REPLY =~ ^[Yy]$ ]]; then
-    read -p "${N1}Do you log in as root using a password? Press "y" for yes and "n" if you use a ssh key...${N1}" -n 1 -r
+    read -s -p "${N1}Do you log in as root using a password? Press "y" for yes and "n" if you use a ssh key...${N1}" -n 1 -r
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-      echo " Run ansible-playbook -k ansible/ansible-install-restic.yml"
+      echo "${N1}Run ansible-playbook -k ansible/ansible-install-restic.yml${N1}"
       exit 0
     else
-      echo "Run ansible-playbook ansible/ansible-install-restic.yml"
+      echo "${N1}Run ansible-playbook ansible/ansible-install-restic.yml${N1}"
       exit 0
     fi
   else
-    read -p "${N1}Do you log in as non-root using a password? Press "y" for yes and "n" if you use a ssh key...${N1}" -n 1 -r
+    read -s -p "${N1}Do you log in as non-root using a password? Press "y" for yes and "n" if you use a ssh key...${N1}" -n 1 -r
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-      echo "Run ansible-playbook -u ADD_USERNAME -k -K -b ansible/ansible-install-restic.yml"
+      echo "${N1}Run ansible-playbook -u ADD_USERNAME -k -K -b ansible/ansible-install-restic.yml${N1}"
       exit 0
     else
-      echo "Run ansible-playbook -u ADD_USERNAME -K -b ansible/ansible-install-restic.yml"
+      echo "${N1}Run ansible-playbook -u ADD_USERNAME -K -b ansible/ansible-install-restic.yml${N1}"
       exit 0
     fi
   fi
-else
-  echo "Continuing with single-server installation... (peasant)"
-fi
-
-echo "Adding 6-hour schedule to cron"
-(crontab -l 2>/dev/null; echo "0 */6 * * * /root/Smoothbrain-OT-Backup/restic-backup.sh") | crontab -
-
-read -p "Press "y" to perform an initial backup now or "n" to exit the installer. The backup IS scheduled to be\
- performed at the next 6am/12pm/6pm/12am time (based on servertime)" -n 1 -r
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  /root/Smoothbrain-OT-Backup/restic-backup.sh
-else
-  echo "Thank you for choosing Smoothbrain OT Backup! Now with more Smoothbrain!"
-  exit 0
 fi
