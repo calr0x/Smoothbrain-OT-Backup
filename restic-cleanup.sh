5ec43c6d247bc48b9f965c983f3cc69d128f8bc0
calr0x <52664000+calr0x@users.noreply.github.com>
2021-06-01 14:05:55 -0500

    Add files via upload

---
+++ restic-cleanup.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+source "/root/OT-Smoothbrain-Backup/config.sh"
+N1=$'\n'
+
+echo "Removing outdated snapshots and data"
+
+FORGET_OUTPUT=`/root/OT-Smoothbrain-Backup/restic forget --group-by host --keep-last 1 2>&1`
+FORGET_STATUS=$?
+echo "$FORGET_OUTPUT"
+
+echo "Notifying result of forget command with telegram STATUS=$FORGET_STATUS"
+
+if [ $FORGET_STATUS == 0 ]; then
+  /root/OT-Smoothbrain-Backup/data/send.sh "Forget command SUCCEEDED"
+else
+  /root/OT-Smoothbrain-Backup/data/send.sh "Forget command FAILED${N1}$FORGET_OUTPUT"
+  exit 1
+fi
+
+PRUNE_OUTPUT=`/root/OT-Smoothbrain-Backup/restic prune 2>&1`
+PRUNE_STATUS=$?
+echo "$PRUNE_OUTPUT"
+
+echo "Notifying result of prune command with telegram STATUS=$PRUNE_STATUS"
+
+if [ $PRUNE_STATUS == 0 ]; then
+  /root/OT-Smoothbrain-Backup/data/send.sh "Prune command SUCCEEDED"
+else
+  /root/OT-Smoothbrain-Backup/data/send.sh "Prune command FAILED${N1}$PRUNE_OUTPUT"
+  exit 1
+fi
+
+CHECK_OUTPUT=`/root/OT-Smoothbrain-Backup/restic check 2>&1`
+CHECK_STATUS=$?
+echo "$CHECK_OUTPUT"
+
+echo "Notifying result of check command with telegram STATUS=$CHECK_STATUS"
+
+if [ $CHECK_STATUS == 0 ]; then
+  /root/OT-Smoothbrain-Backup/data/send.sh "Check command SUCCEEDED"
+else
+  /root/OT-Smoothbrain-Backup/data/send.sh "Check command FAILED${N1}$CHECK_OUTPUT"
+  exit 1
+fi
