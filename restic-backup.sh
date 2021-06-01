4c2fb5fe0a4d5273b1522a1c5537dd2703a20c50
calr0x <52664000+calr0x@users.noreply.github.com>
2021-06-01 11:38:01 -0500

    Add files via upload

--- restic-backup.sh
+++ restic-backup.sh
@@ -14,52 +14,52 @@ echo "Linking container backup folder to /root/Smoothbrain-OT-Backup/backup"
 OUTPUT=$(ln -sf "$(docker inspect --format='{{.GraphDriver.Data.MergedDir}}' otnode)/ot-node/backup" /root/Smoothbrain-OT-Backup/)
 echo $STATUS
 if [ $STATUS == 1 ]; then
-  /root/Smoothbrain-OT-Backup/data/send.sh "Linking container backup folder command FAILED${N1}$OUTPUT"
+  /root/Smoothbrain-OT-Backup/data/send.sh "Linking container backup folder command FAILED"
   exit 1
 fi
 
 
 echo "Backing up OT Node data"
-OUTPUT=$(docker exec otnode node scripts/backup.js --config=/ot-node/.origintrail_noderc --configDir=/ot-node/data --backupDirectory=/ot-node/backup  2>&1)
+docker exec otnode node scripts/backup.js --config=/ot-node/.origintrail_noderc --configDir=/ot-node/data --backupDirectory=/ot-node/backup  2>&1
 echo $STATUS
 if [ $STATUS == 1 ]; then
-  /root/Smoothbrain-OT-Backup/data/send.sh "OT docker backup command FAILED${N1}$OUTPUT"
+  /root/Smoothbrain-OT-Backup/data/send.sh "OT docker backup command FAILED"
   exit 1
 fi
 
 echo "Moving data out of dated folder into backup"
-OUTPUT=$(mv -v /root/Smoothbrain-OT-Backup/backup/202*/* /root/Smoothbrain-OT-Backup/backup/ 2>&1)
+mv -v /root/Smoothbrain-OT-Backup/backup/202*/* /root/Smoothbrain-OT-Backup/backup/ 2>&1
 echo $STATUS
 if [ $STATUS == 1 ]; then
-  /root/Smoothbrain-OT-Backup/data/send.sh "Moving data command FAILED${N1}$OUTPUT"
+  /root/Smoothbrain-OT-Backup/data/send.sh "Moving data command FAILED"
   exit 1
 fi
 
 echo "Moving hidden data out of dated folder into backup"
-OUTPUT=$(mv -v /root/Smoothbrain-OT-Backup/backup/.origintrail_noderc /root/Smoothbrain-OT-Backup/backup/ 2>&1)
+mv -v /root/Smoothbrain-OT-Backup/backup/.origintrail_noderc /root/Smoothbrain-OT-Backup/backup/ 2>&1
 echo $STATUS
 if [ $STATUS == 1 ]; then
-  /root/Smoothbrain-OT-Backup/data/send.sh "Moving data command FAILED${N1}$OUTPUT"
+  /root/Smoothbrain-OT-Backup/data/send.sh "Moving data command FAILED"
   exit 1
 fi
 
 
 echo "Deleting dated folder"
-OUTPUT=$(rm -rf /root/Smoothbrain-OT-Backup/backup/202* 2>&1)
+rm -rf /root/Smoothbrain-OT-Backup/backup/202* 2>&1
 echo $STATUS
 if [ $STATUS == 1 ]; then
-  /root/Smoothbrain-OT-Backup/data/send.sh "Deleting data folder command FAILED${N1}$OUTPUT"
+  /root/Smoothbrain-OT-Backup/data/send.sh "Deleting data folder command FAILED"
   exit 1
 fi
 
 echo "Uploading data to Amazon S3"
-OUTPUT=$(/root/Smoothbrain-OT-Backup/restic backup /root/Smoothbrain-OT-Backup/backup/.origintral_noderc /root/Smoothbrain-OT-Backup/backup/* 2>&1)
+/root/Smoothbrain-OT-Backup/restic backup /root/Smoothbrain-OT-Backup/backup/.origintral_noderc /root/Smoothbrain-OT-Backup/backup/* 2>&1
 echo $STATUS
 if [ $STATUS == 0 ]; then
-  /root/Smoothbrain-OT-Backup/data/send.sh "Backup SUCCESSFUL${N1}$OUTPUT"
+  /root/Smoothbrain-OT-Backup/data/send.sh "Backup SUCCESSFUL"
   rm -rf /root/Smoothbrain-OT-Backup/backup/*
 else
-  /root/Smoothbrain-OT-Backup/data/send.sh "Uploading backup to S3 FAILED${N1}$OUTPUT"
+  /root/Smoothbrain-OT-Backup/data/send.sh "Uploading backup to S3 FAILED"
 fi
 
 exit $STATUS
