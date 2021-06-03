fb2b36be52d6092730be501007d9e4bc94793e72
calr0x <52664000+calr0x@users.noreply.github.com>
2021-06-02 21:57:17 -0500

    Add files via upload

--- restic-backup.sh
+++ restic-backup.sh
@@ -20,8 +20,7 @@ fi
 
 
 echo "Backing up OT Node data"
-OUTPUT=$(docker exec otnode node scripts/backup.js --config=/ot-node/.origintrail_noderc --configDir=/ot-node/data --backupDirectory=/ot-node/backup  2>&1)
-echo $OUTPUT
+docker exec otnode node scripts/backup.js --config=/ot-node/.origintrail_noderc --configDir=/ot-node/data --backupDirectory=/ot-node/backup  2>&1
 echo $STATUS
 if [ $STATUS == 1 ]; then
   /root/OT-Smoothbrain-Backup/data/send.sh "OT docker backup command FAILED"
@@ -40,7 +39,7 @@ echo "Moving hidden data out of dated folder into backup"
 mv -v /root/OT-Smoothbrain-Backup/backup/*/.origintrail_noderc /root/OT-Smoothbrain-Backup/backup/ 2>&1
 echo $STATUS
 if [ $STATUS == 1 ]; then
-  /root/OT-Smoothbrain-Backup/data/send.sh "Moving data command FAILED"
+  /root/OT-Smoothbrain-Backup/data/send.sh "Moving hidden data command FAILED"
   exit 1
 fi
 
@@ -54,10 +53,10 @@ if [ $STATUS == 1 ]; then
 fi
 
 echo "Uploading data to Amazon S3"
-/root/OT-Smoothbrain-Backup/restic backup /root/OT-Smoothbrain-Backup/backup/.origintrail_noderc /root/OT-Smoothbrain-Backup/backup/* 2>&1
+OUTPUT=/root/OT-Smoothbrain-Backup/restic backup /root/OT-Smoothbrain-Backup/backup/.origintrail_noderc /root/OT-Smoothbrain-Backup/backup/* 2>&1
 echo $STATUS
 if [ $STATUS == 0 ]; then
-  /root/OT-Smoothbrain-Backup/data/send.sh "Backup SUCCESSFUL: $OUTPUT"
+  /root/OT-Smoothbrain-Backup/data/send.sh "Backup SUCCESSFUL:${N1}$OUTPUT"
   rm -rf /root/OT-Smoothbrain-Backup/backup/* /root/OT-Smoothbrain-Backup/backup/.origintrail_noderc
 else
   /root/OT-Smoothbrain-Backup/data/send.sh "Uploading backup to S3 FAILED"
