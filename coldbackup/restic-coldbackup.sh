5aa8b1aaa70f340f72b809fdeaad936bfeb4ed8b
calvin <calr0x@gmail.com>
2021-07-09 18:03:13 -0500

    Initial ColdBrain upload

---
+++ coldbackup/restic-coldbackup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+source "/root/OT-Smoothbrain-Backup/config.sh"
+STATUS=$?
+N1=$'\n'
+
+echo "Stopping  otnode"
+systemctl stop otnode
+
+echo "Stopping arangodb3"
+systemctl stop arangodb3
+
+echo "Uploading data to Amazon S3"
+
+OUTPUT=$(/root/OT-Smoothbrain-Backup/restic backup /ot-node/current/.origintrail_noderc /root/.origintrail_noderc /var/lib/arangodb3 /var/lib/arangodb3-apps 2>&1)
+
+if [ $? -eq 0 ]; then
+  /root/OT-Smoothbrain-Backup/data/send.sh "Backup SUCCESSFUL:${N1}$OUTPUT"
+  rm -rf /root/backup/* /root/backup/.origintrail_noderc
+else
+  /root/OT-Smoothbrain-Backup/data/send.sh "Uploading backup to S3 FAILED:${N1}$OUTPUT"
+  systemctl start arangodb3
+  systemctl start otnode
+  exit 1
+fi
+
+systemctl start arangodb3
+systemctl start otnode
+
+exit 0
