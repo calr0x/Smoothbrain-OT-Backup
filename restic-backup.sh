c8aeaa1901e64c6c84d4be40c0c23a36b7c28182
calr0x <52664000+calr0x@users.noreply.github.com>
2021-08-01 08:07:40 -0500

    update restic-backup.sh

--- restic-backup.sh
+++ restic-backup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #Test
 source "/root/OT-Settings/config.sh"
-STATUS=$?
-N1=$'\n'
+STATUS = $?
+N1 = $'\n'
 
-if [ -d "/root/backup" ]; then
-  echo "Deleting existing backup folder"
-  rm -rf /root/backup
+if [-d "/root/backup"]; then
+echo "Deleting existing backup folder"
+rm -rf /root/backup
 fi
 
 ln -s /ot-node/backup /root/backup
@@ -14,50 +14,62 @@ ln -s /ot-node/backup /root/backup
 cd /ot-node/current
 
 echo "Backing up OT Node data"
-OUTPUT=$(node /ot-node/current/scripts/backup.js --config=/ot-node/current/.origintrail_noderc --configDir=/root/.origintrail_noderc/mainnet --backupDirectory=/root/backup  2>&1)
+OUTPUT = $(node /ot-node/current/scripts/backup.js --config=/ot-node/current/.origintrail_noderc --configDir=/root/.origintrail_noderc/mainnet --backupDirectory=/root/backup 2 > &1)
 
-if [ $? -eq 1 ]; then
-  /root/OT-Settings/data/send.sh "OT backup command FAILED:${N1}$OUTPUT"
-  exit 1
+if [$? -eq 1]; then
+/root/OT-Settings/data/send.sh "OT backup command FAILED:$ {
+    N1
+}$OUTPUT"
+exit 1
 fi
 
 echo "Moving data out of dated folder into backup"
-OUTPUT=$(mv -v /root/backup/202*/* /root/backup/ 2>&1)
+OUTPUT = $(mv -v /root/backup/202*/* /root/backup/ 2 > &1)
 
-if [ $? -eq 1 ]; then
-  /root/OT-Settings/data/send.sh "Moving data command FAILED::${N1}$OUTPUT"
-  exit 1
+if [$? -eq 1]; then
+/root/OT-Settings/data/send.sh "Moving data command FAILED::$ {
+    N1
+}$OUTPUT"
+exit 1
 fi
 
 echo "Moving hidden data out of dated folder into backup"
-OUTPUT=$(mv -v /root/backup/*/.origintrail_noderc /root/backup/ 2>&1)
+OUTPUT = $(mv -v /root/backup/*/.origintrail_noderc /root/backup/ 2 > &1)
 
-if [ $? -eq 1 ]; then
-  /root/OT-Settings/data/send.sh "Moving hidden data command FAILED:${N1}$OUTPUT"
-  exit 1
+if [$? -eq 1]; then
+/root/OT-Settings/data/send.sh "Moving hidden data command FAILED:$ {
+    N1
+}$OUTPUT"
+exit 1
 fi
 echo $?
 
 echo "Deleting dated folder"
-OUTPUT=$(rm -rf /root/backup/202* 2>&1)
+OUTPUT = $(rm -rf /root/backup/202* 2 > &1)
 
-if [ $? -eq 1 ]; then
-  /root/OT-Settings/data/send.sh "Deleting data folder command FAILED:${N1}$OUTPUT"
-  exit 1
+if [$? -eq 1]; then
+/root/OT-Settings/data/send.sh "Deleting data folder command FAILED:$ {
+    N1
+}$OUTPUT"
+exit 1
 fi
 echo $?
 
 echo "Uploading data to Amazon S3"
-OUTPUT=$(/root/OT-Smoothbrain-Backup/restic backup /root/backup/.origintrail_noderc /root/backup/* 2>&1)
+OUTPUT = $(/root/OT-Smoothbrain-Backup/restic backup /root/backup/.origintrail_noderc /root/backup/* 2 > &1)
 
-if [ $? -eq 0 ]; then
-  if [ $SMOOTHBRAIN_NOTIFY_ON_SUCCESS == "true" ]; then
-    /root/OT-Settings/data/send.sh "Backup SUCCESSFUL:${N1}$OUTPUT"
-    rm -rf /root/backup/* /root/backup/.origintrail_noderc
-  fi
+if [$? -eq 0]; then
+if [$SMOOTHBRAIN_NOTIFY_ON_SUCCESS == "true"]; then
+/root/OT-Settings/data/send.sh "Backup SUCCESSFUL:$ {
+    N1
+}$OUTPUT"
+rm -rf /root/backup/* /root/backup/.origintrail_noderc
+fi
 else
-  /root/OT-Settings/data/send.sh "Uploading backup to S3 FAILED:${N1}$OUTPUT"
-  exit 1
+    /root/OT-Settings/data/send.sh "Uploading backup to S3 FAILED:$ {
+    N1
+}$OUTPUT"
+exit 1
 fi
 echo $OUTPUT
 
