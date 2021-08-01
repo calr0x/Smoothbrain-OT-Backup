b47401165d4228f0f168ac4ce162405735be404a
calr0x <52664000+calr0x@users.noreply.github.com>
2021-08-01 15:55:26 -0500

    Update restic-backup.sh

--- restic-backup.sh
+++ restic-backup.sh
@@ -17,9 +17,7 @@ echo "Backing up OT Node data"
 OUTPUT = $(node /ot-node/current/scripts/backup.js --config=/ot-node/current/.origintrail_noderc --configDir=/root/.origintrail_noderc/mainnet --backupDirectory=/root/backup 2 > &1)
 
 if [$? -eq 1]; then
-/root/OT-Settings/data/send.sh "OT backup command FAILED:$ {
-    N1
-}$OUTPUT"
+/root/OT-Settings/data/send.sh "OT backup command FAILED:${N1}$OUTPUT"
 exit 1
 fi
 
@@ -27,9 +25,7 @@ echo "Moving data out of dated folder into backup"
 OUTPUT = $(mv -v /root/backup/202*/* /root/backup/ 2 > &1)
 
 if [$? -eq 1]; then
-/root/OT-Settings/data/send.sh "Moving data command FAILED::$ {         
-    N1
-}$OUTPUT"
+/root/OT-Settings/data/send.sh "Moving data command FAILED::${N1}$OUTPUT"
 exit 1
 fi
 
@@ -37,9 +33,7 @@ echo "Moving hidden data out of dated folder into backup"
 OUTPUT = $(mv -v /root/backup/*/.origintrail_noderc /root/backup/ 2 > &1)
 
 if [$? -eq 1]; then
-/root/OT-Settings/data/send.sh "Moving hidden data command FAILED:$ {
-    N1
-}$OUTPUT"
+/root/OT-Settings/data/send.sh "Moving hidden data command FAILED:${N1}$OUTPUT"
 exit 1
 fi
 echo $?
@@ -48,9 +42,7 @@ echo "Deleting dated folder"
 OUTPUT = $(rm -rf /root/backup/202* 2 > &1)
 
 if [$? -eq 1]; then
-/root/OT-Settings/data/send.sh "Deleting data folder command FAILED:$ {
-    N1
-}$OUTPUT"
+/root/OT-Settings/data/send.sh "Deleting data folder command FAILED:${N1}$OUTPUT"
 exit 1
 fi
 echo $?
@@ -60,15 +52,11 @@ OUTPUT = $(/root/OT-Smoothbrain-Backup/restic backup /root/backup/.origintrail_n
 
 if [$? -eq 0]; then
 if [$SMOOTHBRAIN_NOTIFY_ON_SUCCESS == "true"]; then
-/root/OT-Settings/data/send.sh "Backup SUCCESSFUL:$ {
-    N1
-}$OUTPUT"
+/root/OT-Settings/data/send.sh "Backup SUCCESSFUL:${N1}$OUTPUT"
 rm -rf /root/backup/* /root/backup/.origintrail_noderc
 fi
 else
-    /root/OT-Settings/data/send.sh "Uploading backup to S3 FAILED:$ {
-    N1
-}$OUTPUT"
+    /root/OT-Settings/data/send.sh "Uploading backup to S3 FAILED:${N1}$OUTPUT"
 exit 1
 fi
 echo $OUTPUT
